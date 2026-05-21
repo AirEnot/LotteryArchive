8a78f07faec3dccb4df6913984f95b05189ba828
Arseniy Kadochnikov <arseniikad22@gmail.com>
2026-05-21 20:15:07 +0300

    v1.0.4 Lottery tickets update

--- Model/Core/Lottery.Tickets.cs
+++ Model/Core/Lottery.Tickets.cs
@@ -9,59 +9,24 @@ namespace Model.Core
 {
     public partial class Lottery
     {
-        //public string LotteryName { get; set; }
-        public int TicketsPrice { get; private set; }
-        //public int TempVarNextTicketNumber { get; private set; } 
-        public List<Ticket> LotteryTickets {  get; private set; }
+        private int _lastTicketId;
 
         public bool SellTicket(LotteryParticipant participant)
         {
+            if (_lastTicketId + 1 >= TicketsCount)
+            {
+                return false; // No more tickets available
+            }
+
+            if (participant.Balance < TicketsPrice)
+            {
+                return false; // Not enough balance to buy a ticket
+            }
+
+            _lastTicketId++;
+            LotteryTickets[_lastTicketId].SellToParticipant(participant);
+
             return true;
-            //    int maxTickets = 0;
-            //    if (TempVarNextTicketNumber > maxTickets)
-            //    {
-            //        return false;
-            //    }
-            //    if (participant.Balance < TicketsPrice)
-            //    {
-            //        return false;
-            //    }
-            //    Ticket newTicket = new Ticket(TempVarNextTicketNumber)
-            //    {
-            //        Price = TicketPrice
-            //    };
-
-            //    // 4. Увеличиваем счетчик! Следующий билет получит номер +1
-            //    _nextTicketNumber++;
-
-            //    // 5. Передаем билет участнику через наш красивый перегруженный оператор +
-            //    // Внутри оператора у участника спишутся деньги и билет добавится в его список
-            //    participant += newTicket;
-
-            //    // 6. Лотерея забирает корешок билета себе в архив (для будущего розыгрыша)
-            //    SoldTickets.Add(newTicket);
-
-            //    // 7. Если этого человека еще нет в списке участников данной лотереи - добавляем
-            //    if (!Participants.Contains(participant))
-            //    {
-            //        Participants.Add(participant);
-            //    }
-
-            //    return true;
-            //}
-
-            //// 2-й вариант метода (ПЕРЕГРУЗКА): Продажа сразу нескольких билетов
-            //public void SellTicket(LotteryParticipant participant, int count)
-            //{
-            //    for (int i = 0; i < count; i++)
-            //    {
-            //        // Пытаемся продать 1 билет.
-            //        bool success = SellTicket(participant);
-
-            //        // Если SellTicket вернул false (кончились билеты или деньги), 
-            //        // мы просто прерываем цикл.
-            //        if (!success) break;
-            //    }
         }
     }
 }
